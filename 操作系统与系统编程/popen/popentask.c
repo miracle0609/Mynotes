30fa80497915908ab8f20ae6a15aa3884c7e5697
fangsong0517 <2035756541@qq.com>
2020-04-14 23:58:59 +0800

    Change code

--- 操作系统与系统编程/popen/popentask.c
+++ 操作系统与系统编程/popen/popentask.c
@@ -12,41 +12,42 @@
 #include<errno.h>
 #include<stdlib.h>
 #include<sys/wait.h>
-static pid_t *childpid = NULL;
+static pid_t *childpid = NULL;/*存储打开了那些文件，有多少子进程,保存子进程pid*/
 static int max;
-
+/*有可能多次使用popen,所以使用数组childpid*/
 FILE *m_popen(const char *command, const char *type) {
     FILE *fp;
     int pfd[2];
     pid_t pid;
-    
+    /*判断参数是否合规*/
     if((type[0] != 'r' && type[0] != 'w') || type[1] != 0) {
         errno = EINVAL;
         return NULL;
     }
-
-    if(childpid == NULL) {
-        max = sysconf(_SC_OPEN_MAX);
+    /*申请空间*/
+    if(childpid == NULL) {/*第一次用进行申请空间*/
+        max = sysconf(_SC_OPEN_MAX);/*最大开多大 "ulimit -a"*/
         if((childpid = (pid_t *)calloc(max, sizeof(pid_t))) == NULL) 
             return NULL;
     }
+    /*创建管道*/
     if(pipe(pfd) < 0) {
         return NULL;
     }
-
-    if(pfd[0] >= max || pfd[1] >= max) {
+    if(pfd[0] >= max || pfd[1] >= max) {/*读写端如果超了就必须关掉*/
         close(pfd[0]);
         close(pfd[1]);
         return NULL;
     }
+    /*创建进程*/
     if((pid = fork()) < 0) {
         return NULL;
     }
-
+    /*子进程*/
     if(pid == 0) {
         if(type[0] == 'r') {
-            close(pfd[0]);
-            if(pfd[1] != STDOUT_FILENO) {
+            close(pfd[0]);/*把读关掉*/
+            if(pfd[1] != STDOUT_FILENO) {/*如果不是标准输出*/
                 dup2(pfd[1], STDOUT_FILENO);
                 close(pfd[1]);
             }
@@ -57,15 +58,15 @@ FILE *m_popen(const char *command, const char *type) {
                 close(pfd[0]);
             }
         }
-        for(int i = 0; i <= max; i++)
+        for(int i = 0; i <= max; i++)/*父进程不需要了*/
             if(childpid[i] > 0)
             close(i);
         execlp("sh","sh","-c", command, NULL);
         exit(0);
-    } 
-    if(type[0] == 'r') {
+    }
+    if(type[0] == 'r') {/*父进程读*/
         close(pfd[1]);
-        if((fp = fdopen(pfd[0], type)) == NULL) {
+        if((fp = fdopen(pfd[0], type)) == NULL) {/*转换文件流*/
             return NULL;
         }
     } else {
