a4a48d94a87ceb3245d8a821a633275ca4f31c7f
fangsong0517 <2035756541@qq.com>
2020-04-28 09:30:32 +0800

    改进并查集

--- Hzoj_Code/71-朋友圈.cpp
+++ Hzoj_Code/71-朋友圈.cpp
@@ -4,7 +4,7 @@
 	> Mail: 
 	> Created Time: 2020年04月28日 星期二 09时13分31秒
  ************************************************************************/
-
+/*
 #include<iostream>
 #include<cstdio>
 #include<cmath>
@@ -39,3 +39,39 @@ int main() {
     }
     return 0;
 }
+*/
+
+#include<iostream>
+using namespace std;
+const int MAX_N = 10000;
+struct UnionSet{
+    int fa[MAX_N + 5];
+    void init(int n) {
+        for(int i = 1; i <= n; i++) fa[i] = i;
+    }
+    int get(int x) {
+        if(x != fa[x]) fa[x] = get(fa[x]);
+        return fa[x];
+    }
+    void merge(int a, int b) {
+        fa[get(a)] = get(b);
+    }
+
+};
+
+UnionSet u;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    u.init(n);
+    for(int i = 0;i < m; i++) {
+        int a , b , c;
+        cin >> a >> b >> c;
+        switch(a) {
+            case 1:u.merge(b, c);break;
+            case 2:cout << (u.get(b) - u.get(c) ? "No" : "Yes") << endl; break;
+        }
+    }
+    return 0;
+}
+
