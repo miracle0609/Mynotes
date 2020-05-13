acbc1d49951cb8572b5e5f8ace56e39c1b3d207b
fangsong0517 <2035756541@qq.com>
2020-05-13 18:48:04 +0800

    贪心

--- Hzoj_Code/504-删数.cpp
+++ Hzoj_Code/504-删数.cpp
@@ -14,6 +14,7 @@
 #include<map>
 #include<vector>
 using namespace std;
+/*
 string s;
 int main() {
     int t;
@@ -33,5 +34,29 @@ int main() {
         if(s[i] !='0')break;
     }
     cout << s.substr(i) << endl;
+    return 0;
+}
+*/
+
+string s;
+int main() {
+    int n;
+    cin >> s >> n;
+    for(int i = 0; i < n; i++) {
+        int ind = s.size() - 1;
+        for(int j = 0; j < s.size() - 1; j ++) {
+            if(s[j] > s[j + 1]) {
+                ind = j;
+                break;
+            }
+        }
+        s.replace(ind, 1, "");
+    }
+    int flag = 0;
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] != 0)flag = 1;
+        if(flag)cout << s[i];
+    }
+    cout << endl;
     return 0;
 }
